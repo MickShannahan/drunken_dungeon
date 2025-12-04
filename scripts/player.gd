2df28d48ec69c7dddb66a59613eec19bd5d1f9af
mick shannahan <beasticorn@gmail.com>
2025-12-04 13:57:12 -0700

    still messing with the movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const TILE_SIZE: Vector2 = Vector2(16,16)
-var sprite_noe_pos_tween: Tween
+var sprite_node_pos_tween: Tween
+var dice_rolled : int
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +15,21 @@ func _physics_process(delta: float) -> void:
 		_move(Vector2(-1, 0))
 	elif Input.is_action_just_pressed("ui_right") and !$right.is_colliding():
 		_move(Vector2(1, 0))
+	elif Input.is_action_just_pressed("ui_accept"):
+		_roll_dice()
 
 func _move(dir: Vector2):
 	global_position += dir * TILE_SIZE
+	
+#	required for tweening
+	$CharacterSprite.global_position -= dir * TILE_SIZE
+	if sprite_node_pos_tween:
+			sprite_node_pos_tween.kill()
+	sprite_node_pos_tween = create_tween()
+	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	sprite_node_pos_tween.tween_property($CharacterSprite, "global_position", global_position, .15).set_trans(Tween.TRANS_SINE)
+	
+func _roll_dice():
+	var random = RandomNumberGenerator.new()
+	dice_rolled = random.randi_range(1,8)
+	print_debug('🎲', dice_rolled)
