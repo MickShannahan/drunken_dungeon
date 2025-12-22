2eb56187291c46df7e29558997e98593ee1b95d4
mick shannahan <beasticorn@gmail.com>
2025-12-22 08:03:25 -0700

    bug with moving down left

--- scripts/move.gd
+++ scripts/move.gd
@@ -54,7 +54,7 @@ func Update(_delta: float):
 			_move_diagonal(directions.up_left)
 		elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"): # ↘️
 			_move_diagonal(directions.down_right)
-		elif Input.is_action_pressed("ui_down") and Input.is_action_just_pressed("ui_left"): # ↙️
+		elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"): # ↙️
 			_move_diagonal(directions.down_left)
 
 func _move_cardinal(dir: Vector2):
